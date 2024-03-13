0ed413beec1cb580acdbc22d9a9a76d8abc05591
KHayotova1 <sitorahayotova7455@mail.ru>
2024-03-13 16:56:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// let i = 9;
+// do {
+//     i++;
+//     console.log(i);
+// }while (i < 30)
+
+// for (let i =3 ; i<6; i++){
+//     console.log(i);
+// }
+// for (let i =0 ; i<41; i++){
+//     console.log(i);
+// }
+
+
+
+// let array = [2,223,3,4,,5,56,77,88,98,90,99];
+// for (let i = 0; i < array.length; i++){
+//     console.log(array[i]);
+// }
+
+// let array = [2,223,3,4,,5,56,77,88,98,90,99,true,'dadrfq'];
+// for (let i = 0; i < array.length; i++){
+//     if ( typeof array[i] == "string")
+//     console.log(array[i]);
+
+// }
+// let array = [2,223,3,4,,5,56,77,88,98,90,99,true,'dadrfq'];
+// for (let i = 0; i < array.length; i++){
+//     if ( typeof array[i] == "number")
+//     console.log(array[i]);
+
+// }
+
+// let array = [2,223,3,4,,5,56,77,88,98,90,99,true,'dadrfq'];
+// for (let i = 0; i < array.length; i++){
+//     if ( array[i] % 2 ==0)
+//     console.log(array[i]);
+
+// }
